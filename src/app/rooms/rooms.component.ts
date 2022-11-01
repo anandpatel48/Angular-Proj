e0f0c31730882202689828a740c5365255c8ed92
anandpatel48 <anandrpatel48@gmail.com>
2022-10-31 18:23:27 -1000

    lifecycle

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -13,49 +13,77 @@ export class RoomsComponent implements OnInit {
 
   hideRooms = false;
 
+  selectedRoom!: RoomList
+
+  title = 'Room List'
+
   rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
     bookedRooms: 5
   }
 
-  roomList: RoomList[] = [
-    {
-      roomNumber: 1,
+  roomList: RoomList[] = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 500,
+        photos: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('12-Nov-2022'),
+        rating: 4.2
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 1000,
+        photos: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('13-Nov-2022'),
+        rating: 3.4
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 1500,
+        photos: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('13-Nov-2022'),
+        rating: 4.7
+      }
+    ]
+  }
+
+  toggle() {
+    this.hideRooms = !this.hideRooms
+    this.title = "Rooms List"
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 4,
       roomType: 'Deluxe Room',
       amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
       price: 500,
       photos: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
       checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 1000,
-      photos: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-      checkinTime: new Date('11-Nov-2022'),
       checkoutTime: new Date('13-Nov-2022'),
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Private Suite',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 1500,
-      photos: 'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('13-Nov-2022'),
+      rating: 4.5,
     }
-  ]
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  toggle() {
-    this.hideRooms = !this.hideRooms
+    // this.roomList.push(room)
+    this.roomList = [...this.roomList, room]
   }
 
 }
